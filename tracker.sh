02dff7f873b252153c1bfaa30e17fec0ae5ae9e9
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-23 16:31:15 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -2,18 +2,19 @@
 
 # Check if this computer is a Raspberry Pi
 is_raspi=false
-# If the kernel release contains
+# Check if this computer is a Raspberry Pi (running GNU/Linux, not Android)
 if [[ $(uname -r) == *"v7l+"* ]] && [[ $(uname -m) == "armv7l" ]] && [[ $(uname -o) == "GNU/Linux" ]]; then
 	is_raspi=true
 fi
 
 echo "Is Raspberry Pi: $is_raspi"
 
-# Use mysql -u root if the script is running on Raspberry Pi
 login_MySQL="mysql -u root"
+# If the script is not running on Raspberry Pi
 if [ $is_raspi = false ] ; then
 	login_MySQL="/opt/lampp/bin/${login_MySQL}"
 fi
+echo '$login_MySQL'
 
 $login_MySQL<<EOF
 	CREATE DATABASE IF NOT EXISTS weatherJB;
