fa83ff6dc3778853b4cedac60cfe4c889e7223df
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-23 16:28:12 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -9,21 +9,21 @@ fi
 
 echo "Is Raspberry Pi: $is_raspi"
 
-: '
-CPU_temp_raw=$(cat /sys/class/thermal/thermal_zone0/temp)
-CPU_temp_c=$(echo "scale=2;$CPU_temp_raw / 1000" | bc)
-echo "CPU Temperature: $CPU_temp_c °C"
-'
+# Use mysql -u root if the script is running on Raspberry Pi
+login_MySQL="mysql -u root"
+if [ $is_raspi = false ] ; then
+	login_MySQL="/opt/lampp/bin/${login_MySQL}"
+fi
 
-# Use mysql -u root if the environment is set, or the script is running on Raspberry Pi
-#/opt/lampp/bin/mysql -u root
-mysql -u root<<EOF
+login_MySQL<<EOF
 	CREATE DATABASE IF NOT EXISTS weatherJB;
 	USE weatherJB;
 EOF
 
 if [ $is_raspi = true ] ; then
-    echo 'Test'
+	CPU_temp_raw=$(cat /sys/class/thermal/thermal_zone0/temp)
+	CPU_temp_c=$(echo "scale=2;$CPU_temp_raw / 1000" | bc)
+	echo "CPU Temperature: $CPU_temp_c °C"
 fi
 
 mysql -u root -e "SHOW DATABASES;"
