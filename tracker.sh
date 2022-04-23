15d3bd84feb051d36947e25be996ce357ac8623e
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-23 11:48:46 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-temp_raw=$(cat /sys/class/thermal/thermal_zone0/temp)
-temp_c =$(echo "scale=2;$temp_raw / 1000" | bc)
-echo "Temperature: $temp_c °C"
+CPU_temp_raw=$(cat /sys/class/thermal/thermal_zone0/temp)
+CPU_temp_c =$(echo "scale=2;$CPU_temp_raw / 1000" | bc)
+echo "Temperature: $CPU_temp_c °C"
 
 # Use mysql -u root if the environment is set, or the script is running on Raspberry Pi
 #/opt/lampp/bin/mysql -u root
