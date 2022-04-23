47694e3462e62057edebdb973f1500ce269a537b
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-23 11:36:43 +0800

    Use mysql -u root

--- tracker.sh
+++ tracker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#/opt/lampp/bin/mysql -u root
 # Use mysql -u root if the environment is set, or the script is running on Raspberry Pi
+#/opt/lampp/bin/mysql -u root
 mysql -u root<<EOF
 	CREATE DATABASE IF NOT EXISTS weatherJB;
 	SHOW DATABASES;
