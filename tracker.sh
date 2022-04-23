07169bfa7b27a4eb120d2ae00d78f54d5a4d170d
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-23 17:52:15 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -33,9 +33,8 @@ if [ $is_raspi = true ] ; then
     	DateTime DateTime NOT NULL,\
     	PRIMARY KEY (ID)\
 	);\
-	IF NOT EXISTS (select * from cpuTemp) THEN\
+	IF NOT EXISTS (select * from cpuTemp)\
 		ALTER TABLE cpuTemp AUTO_INCREMENT = 1;\
-	END IF;\
 	"
 fi
 
