82883087855fa30db3e1c2ab48c1a97b2c2a6933
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-23 11:34:02 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/opt/lampp/bin/mysql -u root<<EOF
+#/opt/lampp/bin/mysql -u root
+# Use mysql -u root if the environment is set, or the script is running on Raspberry Pi
+mysql -u root<<EOF
 	CREATE DATABASE IF NOT EXISTS weatherJB;
 	SHOW DATABASES;
 	USE weatherJB;
