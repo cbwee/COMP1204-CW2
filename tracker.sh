d978ee4a25f1753e3a2cdb0af4694b5a6710a27e
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-24 10:14:14 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -18,7 +18,9 @@ reset_auto_increment_if_empty() {
 	rai_count=$($login_MySQL -e "USE $1; select COUNT(*) from $2;" | tail -n 1)
 	if [ "$rai_count" -eq "0" ]; then
 		echo "Table $2 is empty"
-		$login_MySQL -e "USE $1; ALTER TABLE $2 AUTO_INCREMENT = 1;"
+		rai_initial_count=1
+		$login_MySQL -e "USE $1; ALTER TABLE $2 AUTO_INCREMENT = $rai_initial_count;"
+		echo "Auto increment value is reset to $rai_initial_count"
 	else
 		echo "Table $2 is not empty"
 	fi
