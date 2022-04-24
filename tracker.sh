42ccba7d02c63ca4bc985e8c5f536cdb1203ca2b
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-24 18:44:47 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -207,13 +207,13 @@ done
 if [ "$unit_temp" = "C" ]; then
 	if [ $append_data == true ]; then
 		echo "Start inserting data"
-		$login_MySQL -e "USE $db_name;\
+		$login_MySQL -e 'USE $db_name;\
 		
 		# Insert current weather\
 		#INSERT INTO ${tableArr[0]}(Date, Temp, RealFeel, Phrase, Time, RealFeelShade, AQI, AirQuality, DateTime) \
 		#VALUES ("$current_date", $current_temp, $current_realFeel, "$current_phrase", "$current_time", $current_realFeelShade, $current_aqi, "$current_air_quality", NOW());\
-		"
-		echo "VALUES ("$current_date", $current_temp, $current_realFeel, "$current_phrase", "$current_time", $current_realFeelShade, $current_aqi, "$current_air_quality", NOW());\"
+		'
+		echo 'VALUES ("$current_date", $current_temp, $current_realFeel, "$current_phrase", "$current_time", $current_realFeelShade, $current_aqi, "$current_air_quality", NOW());\'
 		echo "Data inserted"
 	fi
 elif ["$unit_temp" = "F" ]; then
