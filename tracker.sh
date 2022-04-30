37c2635b249dd39e475caa9d855fba1df846b764
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-04-30 10:15:28 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -56,6 +56,7 @@ display_last_insert() {
 	# Use a local variable
 	local count=$($login_MySQL -e "USE $1; SELECT COUNT($3) FROM $2;" | tail -n 1)
 	if [ $count -ne 0 ]; then
+		echo $count
 		local last=$($login_MySQL -e "USE $1; SELECT $3 FROM $2 ORDER BY ID DESC LIMIT 1;" | tail -n 1)
 		echo "Last Data Insertion: $last"
 		echo
