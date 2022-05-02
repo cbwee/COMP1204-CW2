e88e0941ee8d2def5d253cb3ba4063e374d8b101
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-05-02 14:06:11 +0800

    Create plot.sh with variables

---
+++ plot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo
+
+# Assign variables
+db_name="weather_jb"
+tableArr=("current" "tomorrow" )
+
+is_raspi=false
+# Check if this computer is a 64-bit/32-bit Raspberry Pi (running GNU/Linux, not Android)
+if [[ $(uname -r) == @(*"v8+"|*"v7l+") ]] && [[ $(uname -m) == @("aarch64"|"armv7l") ]] && [[ $(uname -o) == "GNU/Linux" ]]; then
+	is_raspi=true
+fi
+echo "Raspberry Pi: $is_raspi"
