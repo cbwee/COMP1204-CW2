f34ef4998768d59886beb70618d0b69fdff6ea1b
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-05-03 17:58:02 +0800

    Added comments

--- plot.sh
+++ plot.sh
@@ -14,7 +14,9 @@ x_dt="set xlabel 'Date and Time'"
 y_tc="set ylabel 'Temperature (°C)'"
 
 directory=$HOME
+# Store temporary files in RAM
 temp="/dev/shm/"
+# File names
 current_fn="${temp}${tableArr[0]}_temperature_data_plot.txt"
 avgc_fn="${temp}average_${tableArr[0]}_plot.txt"
 avgt_fn="${temp}average_${tableArr[1]}_plot.txt"
@@ -24,19 +26,18 @@ is_raspi=false
 if [[ $(uname -r) == @(*"v8+"|*"v7l+") ]] && [[ $(uname -m) == @("aarch64"|"armv7l") ]] && [[ $(uname -o) == "GNU/Linux" ]]; then
 	is_raspi=true
 fi
-# echo "Raspberry Pi: $is_raspi"
 
 login_MySQL="mysql -u root"
 # If the script is not running on Raspberry Pi
 if [ $is_raspi = false ] ; then
 	login_MySQL="/opt/lampp/bin/${login_MySQL}"
 fi
-# echo -e "\nCommand for MySQL login: $login_MySQL\n"
 
 # Plot 1
 current_data=$($login_MySQL -e "USE $db_name;\
 	SELECT DateTime, Temp, RealFeel FROM ${tableArr[0]};")
 	
+# Store the data in a file	
 echo "# $current_data" > $current_fn
 	
 gnuplot <<- EOF
@@ -45,23 +46,29 @@ gnuplot <<- EOF
 	$y_tc
 	$xtime
 	$xyfonts
+	# tics for every degree
 	set ytics 1
 	set key font ',16'
+	# tics for every 12 hours
 	set xtics 60*60*12;
+	# set range from 25 to 41 degrees
 	set yrange[25:41]
+	# set range starting from 0 days ago
 	set xrange [time(0) - 9*24*60*60:]
 	$png_size
 	set output "${directory}/${plotName[0]}"
+	# set line colours
 	set style line 1 lc rgb "#93c701"
 	set style line 2 lc rgb "#f05514"	
 	plot "$current_fn" using 1:3 with lines title "Temperature" ls 1, \
 	"$current_fn" using 1:4 with lines title "RealFeel" ls 2
 EOF
-	
+# Delete the file	
 rm $current_fn	
 	
 # Plot 2
 
+# Names of views
 cavg="${tableArr[0]}_avg"
 tavg="${tableArr[1]}_avg"
 
@@ -71,6 +78,7 @@ avgc=$($login_MySQL -e "USE $db_name;\
 avgt=$($login_MySQL -e "USE $db_name;\
 	SELECT Date, High, Low, Average FROM $tavg;")
 
+# Save data to files
 echo "# $avgc" > $avgc_fn
 echo "# $avgt" > $avgt_fn
 	
@@ -81,13 +89,17 @@ gnuplot <<- EOF
 	$xyfonts
 	set xtics font ',16'
 	set xtics 60*60*24
+	# hide minor tics
 	set xtics scale 1,0
-	set ytics 0.5	
+	# tics for every 0.5 degree
+	set ytics 0.5
 	set yrange[24:35]
+	# range from 11 days ago until now
 	set xrange [time(0) - 11*24*60*60:time(0)]
 	set key font ',14'
 	$png_size
 	set output "${directory}/${plotName[1]}"
+	# set line width and point type
 	set style line 1 lc rgb "#ff1744" lw 2 pt 5
 	set style line 2 lc rgb "#00b0ff" lw 2 pt 5
 	set style line 3 lc rgb "#00e676" lw 2 pt 5
@@ -103,12 +115,12 @@ gnuplot <<- EOF
 	"$avgt_fn" using 1:4 with linespoints title "Tomorrow Average" ls 6
 EOF
 
+# Delete the files
 rm $avgc_fn
 rm $avgt_fn
 
 # Plot 3, only for Raspberry Pi
 if [ $is_raspi = true ]; then
-
 	cpu_fn="${temp}cpu_data_plot.txt"
 	
 	cpu_data=$($login_MySQL -e "USE cputemp;\
@@ -124,7 +136,9 @@ gnuplot <<- EOF
 	$xyfonts
 	set xtics font ',10'
 	set xrange [time(0) - 7*24*60*60:]
+	# tics for every 6 hours
 	set xtics 60*60*6;
+	# hide minor tics
 	set xtics scale 1,0
 	set ytics 1	
 	$png_size
@@ -135,7 +149,7 @@ EOF
 	rm $cpu_fn
 fi
 
-echo "Plots generated, saved in $directory"
+echo "Plots generated, saved in directory: $directory"
 
 for i in "${plotName[@]}"
 do
