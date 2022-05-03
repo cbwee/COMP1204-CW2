a9710b4e04ebdaec3b3dfe822b478ecb9dccfa86
cbw1a19 <cbw1a19@soton.ac.uk>
2022-05-03 11:54:24 +0800

    Still adding the last plot to the script

--- plot.sh
+++ plot.sh
@@ -5,6 +5,7 @@ echo
 # Assign variables
 db_name="weather_jb"
 tableArr=("current" "tomorrow" )
+plotName=("1._Temperature_and_RealFeel.png" "2._Average_Temperature" "3._CPU_Temperature.png")
 
 png_size="set term png size 2160,1080 font ,20"
 xytics="set xtics font ',12'; set ytics font ',14'; set xtics 60*60*12; set ytics 1"
@@ -13,10 +14,21 @@ x_dt="set xlabel 'Date and Time'"
 y_tc="set ylabel 'Temperature (°C)'"
 y_range="set yrange[25:41]"
 
+directory=$HOME
 temp="/dev/shm/"
-cpu_fn="${temp}temp_cpu_data.txt"
-ct_fn="${temp}ct_data.txt"
-crf_fn="${temp}crf_data.txt"
+
+cpu_fn="${temp}cpu_data_plot.txt"
+
+ct_fn="${temp}current_temperature_data_plot.txt"
+crf_fn="${temp}current_realfeel_data_plot.txt"
+
+avgch_fn="${temp}average_current_high_plot.txt"
+avgcl_fn="${temp}average_current_low_plot.txt"
+avgcavg_fn="${temp}average_current_average_plot.txt"
+
+avgth_fn="${temp}average_tomorrow_high_plot.txt"
+avgtl_fn="${temp}average_tomorrow_low_plot.txt"
+avgtavg_fn="${temp}average_tomorrow_average_plot.txt"
 
 is_raspi=false
 # Check if this computer is a 64-bit/32-bit Raspberry Pi
@@ -32,32 +44,81 @@ if [ $is_raspi = false ] ; then
 fi
 echo -e "\nCommand for MySQL login: $login_MySQL\n"
 
+# Plot 1
 current_temp=$($login_MySQL -e "USE $db_name;\
 	SELECT DateTime, Temp FROM ${tableArr[0]};")
 
 current_RF=$($login_MySQL -e "USE $db_name;\
 	SELECT DateTime, RealFeel FROM ${tableArr[0]};")
 	
-	echo "# $current_temp" > $ct_fn
-	echo "# $current_RF" > $crf_fn
+echo "# $current_temp" > $ct_fn
+echo "# $current_RF" > $crf_fn
 	
 gnuplot <<- EOF
 	set title "Temperature and RealFeel"
-	set xlabel "Date and Time"
-	set ylabel "Temperature (°C)"
+	$x_dt
+	$y_tc
 	$xtime
 	$xytics	
+	$y_range
 	$png_size
-	set output "$HOME/1._Temperature_and_RealFeel.png"
+	set output "${directory}/${plotName[0]}"
 	set style line 1 lc rgb "#93c701"
 	set style line 2 lc rgb "#f05514"	
-	plot "$ct_fn" using 1:3 with lines title "Temperature" ls 1, "$crf_fn" using 1:3 with lines title "RealFeel" ls 2
+	plot "$ct_fn" using 1:3 with lines title "Temperature" ls 1, \
+	"$crf_fn" using 1:3 with lines title "RealFeel" ls 2
 EOF
-	#rm $ct_fn
-	#rm $srf_fn
+	
+	
+# Plot 2
+cavg="${tableArr[0]}_avg"
+tavg="${tableArr[1]}_avg"
+
+avgch=$($login_MySQL -e "USE $db_name;\
+	SELECT DateTime, High FROM $cavg;")
+
+avgcl=$($login_MySQL -e "USE $db_name;\
+	SELECT DateTime, Low FROM $cavg;")
+	
+avgcavg=$($login_MySQL -e "USE $db_name;\
+	SELECT DateTime, Average FROM $cavg;")
+	
+echo "# $avgch" > $avgch_fn
+echo "# $avgcl" > $avgcl_fn
+echo "# $avgcavg" > $avgcavg_fn
+echo "# $avgth" > $avgth_fn
+echo "# $avgtl" > $avgtl_fn
+echo "# $avgtavg" > $avgtavg_fn
+	
+	
+gnuplot <<- EOF
+	set title "Average temperature"
+	$x_dt
+	$y_tc
+	$xtime
+	$xytics	
+	$y_range
+	$png_size
+	set output "${directory}/${plotName[1]}"
+	set style line 1 lc rgb "#93c701"
+	set style line 2 lc rgb "#f05514"	
+	plot "$ct_fn" using 1:3 with lines title "Temperature" ls 1, \
+	"$crf_fn" using 1:3 with lines title "RealFeel" ls 2
+EOF
+	
+: '
+rm $ct_fn
+rm $srf_fn
+rm $avgch_fn
+rm $avgcl_fn
+rm $avgcavg_fn
+rm $avgth_fn
+rm $avgtl_fn
+rm $avgtavg_fn
+'
 
 
-
+# Plot 3, only for Raspberry Pi
 if [ $is_raspi = true ]; then
 
 	cpu_data=$($login_MySQL -e "USE cputemp;\
@@ -72,7 +133,7 @@ gnuplot <<- EOF
 	$xtime
 	$xytics	
 	$png_size
-	set output "$HOME/3._CPU_Temperature.png"
+	set output "${directory}/${plotName[2]}"
 	set style line 1 lc rgb "#cc2455"
 	plot "$cpu_fn" using 1:3 with lines notitle ls 1
 EOF
