5a2d3e9263612f8e933820646dcab8eef924b17e
cbw1a19 <cbw1a19@soton.ac.uk>
2022-05-03 16:30:07 +0800

    Commit before making small changes, currently plotting works

--- plot.sh
+++ plot.sh
@@ -5,21 +5,20 @@ echo
 # Assign variables
 db_name="weather_jb"
 tableArr=("current" "tomorrow" )
-plotName=("1._Temperature_and_RealFeel.png" "2._Average_Temperature" "3._CPU_Temperature.png")
+plotName=("1._Temperature_and_RealFeel.png" "2._Average_Temperature.png" "3._CPU_Temperature.png")
 
-png_size="set term png size 2160,1080 font ,20"
-xytics="set xtics font ',12'; set ytics font ',14'; set xtics 60*60*12; set ytics 1"
+png_size="set term png size 2160,1080 font 'times new roman,20'"
+xyfonts="set xtics font ',12'; set ytics font ',14'"
 xtime="set xdata time; set timefmt '%Y-%m-%d %H:%M:%S'; set format x \"%Y-%m-%d\n%H:%M\""
 xdate="set xdata time; set timefmt '%m/%d/%Y'; set format x '%d %B %Y'; set xlabel 'Date'"
 x_dt="set xlabel 'Date and Time'"
 y_tc="set ylabel 'Temperature (°C)'"
-y_range="set yrange[25:41]"
 
 directory=$HOME
 temp="/dev/shm/"
 current_fn="${temp}${tableArr[0]}_temperature_data_plot.txt"
 avgc_fn="${temp}average_${tableArr[0]}_plot.txt"
-acgt_fn="${temp}average_${tableArr[1]}_plot.txt"
+avgt_fn="${temp}average_${tableArr[1]}_plot.txt"
 
 is_raspi=false
 # Check if this computer is a 64-bit/32-bit Raspberry Pi
@@ -46,8 +45,12 @@ gnuplot <<- EOF
 	$x_dt
 	$y_tc
 	$xtime
-	$xytics	
-	$y_range
+	$xyfonts
+	set ytics 1
+	set key font ',16'
+	set xtics 60*60*12;
+	set yrange[25:41]
+	set xrange [time(0) - 9*24*60*60:]
 	$png_size
 	set output "${directory}/${plotName[0]}"
 	set style line 1 lc rgb "#93c701"
@@ -59,7 +62,7 @@ EOF
 # Plot 2
 
 cavg="${tableArr[0]}_avg"
-tavg="${tableArr[0]}_avg"
+tavg="${tableArr[1]}_avg"
 
 avgc=$($login_MySQL -e "USE $db_name;\
 	SELECT Date, High, Low, Average FROM $cavg;")
@@ -69,28 +72,36 @@ avgt=$($login_MySQL -e "USE $db_name;\
 
 echo "# $avgc" > $avgc_fn
 echo "# $avgt" > $avgt_fn
-
 	
 gnuplot <<- EOF
 	set title "Average temperature"
 	$y_tc
 	$xdate
-	$xytics	
-	$y_range
+	$xyfonts
+	set xtics font ',16'
+	set xtics 60*60*24
+	
+	set ytics 0.5	
+	set yrange[24:35]
+	set xrange [time(0) - 9*24*60*60:]
+	set key font ',14'
 	$png_size
 	set output "${directory}/${plotName[1]}"
-	set style line 1 lc rgb "#93c701"
-	set style line 2 lc rgb "#f05514"	
-	plot "$avgc_fn" using 1:2 with lines title "High" ls 1, \
+	set style line 1 lc rgb "#ff1744" lw 2
+	set style line 2 lc rgb "#00b0ff" lw 2
+	set style line 3 lc rgb "#00e676" lw 2
+	set style line 4 lc rgb "#ff616f"
+	set style line 5 lc rgb "#69e2ff"
+	set style line 6 lc rgb "#66ffa6"
+	plot \
+	"$avgc_fn" using 1:2 with lines title "High" ls 1, \
 	"$avgc_fn" using 1:3 with lines title "Low" ls 2, \
-	"$avgc_fn" using 1:4 with lines title "Average", \
-	"$avgt_fn" using 1:2 with lines title "Tomorrow High", \
-	"$avgt_fn" using 1:3 with lines title "Tomorrow Low", \
-	"$avgt_fn" using 1:4 with lines title "Tomorrow Average"
-	
+	"$avgc_fn" using 1:4 with lines title "Average" ls 3, \
+	"$avgt_fn" using 1:2 with lines title "Tomorrow High" ls 4, \
+	"$avgt_fn" using 1:3 with lines title "Tomorrow Low" ls 5, \
+	"$avgt_fn" using 1:4 with lines title "Tomorrow Average" ls 6
 EOF
 
-: '
 
 # Plot 3, only for Raspberry Pi
 if [ $is_raspi = true ]; then
@@ -107,7 +118,11 @@ gnuplot <<- EOF
 	$x_dt
 	$y_tc
 	$xtime
-	$xytics	
+	$xyfonts
+	set xtics font ',10'
+	set xrange [time(0) - 7*24*60*60:]
+	set xtics 60*60*6;
+	set ytics 1	
 	$png_size
 	set output "${directory}/${plotName[2]}"
 	set style line 1 lc rgb "#cc2455"
@@ -115,4 +130,3 @@ gnuplot <<- EOF
 EOF
 	rm $cpu_fn
 fi
-'
