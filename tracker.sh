b14c52233a5c315789072d3b9c330ae5e3db4a1b
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2022-05-04 10:47:57 +0800

    Changed the text for flags

--- tracker.sh
+++ tracker.sh
@@ -14,7 +14,7 @@ elif [ "$1" == "-d" ]; then
 elif [ "$1" == "-p" ]; then
 	plot_files=true
 else
-  echo -e "(Use the -a flag if you want to append the data to MySQL database)\n(Use the -p flag if you want to generate plots)\n"
+  echo -e "Use -a to append the data to database\nUse -d to display the last $num_display rows of data\nUse -p to generate plots\n"
 fi
 echo "Append Data: $append_data"
 
